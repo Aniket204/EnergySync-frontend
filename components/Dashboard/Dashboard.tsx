d1f280d1d804ff965dd9ce2cbc48e085a1413faf
Aniket Mehendale <aniket@Anikets-MacBook-Air.local>
2025-03-13 02:14:24 +0530

    added tiles on dashboard

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Chart } from './Chart';
+import Tile from './Tile';
+import { DollarSign, Fuel, CirclePlay, Wind } from "lucide-react";
 
 const Dashboard = () => {
     return (
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div className="aspect-video rounded-xl bg-muted/50" />
-                <div className="aspect-video rounded-xl bg-muted/50" />
-                <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Tile
+                    title="Money saved"
+                    amount="$45,231.89"
+                    caption="+65% from last month"
+                    Icon={DollarSign}
+                />
+                <Tile
+                    title="Fuel saved"
+                    amount="20.6 Ltr"
+                    caption="+10.1% from last month"
+                    Icon={Fuel}
+                />
+                <Tile
+                    title="CO2 saved"
+                    amount="305 Kg"
+                    caption="+2% from last month"
+                    Icon={Wind}
+                />
+
+                <Tile
+                    title="Total Runtime"
+                    amount="420 Hr"
+                    caption="-8% from last month"
+                    Icon={CirclePlay}
+                />
             </div>
-            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
             <Chart />
         </div>
     )
---
+++ components/Dashboard/Tile.tsx
@@ -0,0 +1,28 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { LucideIcon } from "lucide-react";
+
+type TileProps = {
+  title: string;
+  amount: string;
+  caption: string;
+  Icon: LucideIcon;
+};
+
+const Tile = ({ title, amount, caption, Icon }: TileProps) => {
+  return (
+    <Card className="shadow-base rounded-lg border bg-card text-card-foreground">
+      <CardContent>
+        <div className="flex flex-row items-center justify-between pb-2">
+          <h3 className="font-bold tracking-tight text-sm">{title}</h3>
+          <Icon className="h-4 w-4 text-muted-foreground" />
+        </div>
+        <div className="pt-0">
+          <div className="text-2xl font-bold">{amount}</div>
+          <p className="text-xs text-muted-foreground">{caption}</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Tile;
